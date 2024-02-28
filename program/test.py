2ac88188270a89756c5105164ac94084f2542b46
Milan Horínek <horinek@milanfon.cz>
2024-02-28 20:52:18 +0100

    Working python script

--- program/test.py
+++ program/test.py
@@ -1,7 +1,24 @@
 import os
+import subprocess
+import time
+
+DIRECTORY = "./tbuis/"
 
 # List variants of WAR files
-file_list = [file for file in os.listdir("./tbuis/") if file.startswith('defect-')]
+file_list = [file for file in os.listdir(DIRECTORY) if file.startswith('defect-')]
 file_list.sort()
 
 print(file_list)
+
+# Running Docker Compose
+
+for file in file_list:
+   env = os.environ.copy() 
+   env["WAR_FILE_PATH"] = file
+   os.chdir(DIRECTORY)
+   subprocess.run(["docker-compose", "up", "-d"], env=env)
+   break
+
+time.sleep(30)
+
+subprocess.run(["docker-compose", "down", "--rmi", "all"])

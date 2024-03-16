38c7bb05c21fe81a9081e202cfd470c525ecc70c
Milan Horínek <horinek@milanfon.cz>
2024-03-16 08:44:19 +0100

    Added new test creation to script

--- program/test.py
+++ program/test.py
@@ -2,29 +2,56 @@ import os
 import subprocess
 import time
 import robot
+import argparse
 
 DIRECTORY = "./tbuis/"
+INPUT_FOLDER = "./input/"
+SYSTEM_PROMPT = "./templates/system.txt"
 
-# List variants of WAR files
-file_list = [file for file in os.listdir(DIRECTORY) if file.startswith('defect-')]
-file_list.sort()
+parser = argparse.ArgumentParser(description="Robot Framework test generator.")
+parser.add_argument('-r', '--run', action='store_true', help='Run the generation')
+parser.add_argument('-i', '--input', type=str, help='Create new input file (test)')
+args = parser.parse_args()
 
-print(file_list)
+def new_input(input_name):
+    if not os.path.exists(INPUT_FOLDER):
+        os.makedirs(INPUT_FOLDER)
+    input_file_path = os.path.join(INPUT_FOLDER, input_name)
+    with open(input_file_path, 'w') as new_file:
+        if os.path.exists(SYSTEM_PROMPT):
+            with open(SYSTEM_PROMPT, 'r') as system_prompt:
+                new_file.write(system_prompt.read())
+            print(f"New input file '{input_file_path}' created!")
+        else:
+            print(f"System prompt '{SYSTEM_PROMPT}' not found!")
 
-# Running Docker Compose
+def run(): 
+    # List variants of WAR files
+    file_list = [file for file in os.listdir(DIRECTORY) if file.startswith('defect-')]
+    file_list.sort()
 
-os.chdir(DIRECTORY)
-i = 0
-for file in file_list:
-   print("Deploying: "+file)
-   env = os.environ.copy() 
-   env["WAR_FILE_PATH"] = "./"+file
-   subprocess.run(["docker-compose", "up", "-d", "--build"], env=env)
-   time.sleep(10)
-   robot.run("../rf-test/TBUIS1.robot")
-   subprocess.run(["docker-compose", "down"])
-   if i == 2:
-        break
-   i += 1
+    print(file_list)
 
-subprocess.run(["docker-compose", "down", "--rmi", "all"])
+    # Running Docker Compose
+
+    os.chdir(DIRECTORY)
+    i = 0
+    for file in file_list:
+       print("Deploying: "+file)
+       env = os.environ.copy() 
+       env["WAR_FILE_PATH"] = "./"+file
+       subprocess.run(["docker-compose", "up", "-d", "--build"], env=env)
+       time.sleep(10)
+       robot.run("../rf-test/TBUIS1.robot")
+       subprocess.run(["docker-compose", "down"])
+       if i == 2:
+            break
+       i += 1
+
+    subprocess.run(["docker-compose", "down", "--rmi", "all"])
+
+if __name__ == "__main__":
+    if args.run:
+        run()
+    if args.input:
+        new_input(args.input)

e38cc71009e535ab1dbdeddd69b622d24f26d865
Milan Horínek <horinek@milanfon.cz>
2024-04-08 22:19:16 +0200

    Better statistics

--- program/stats.py
+++ program/stats.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import seaborn as sns
 import re
+from configuration import POSITIVE_FAILS
 
 db_path = '../results/codellama-run.sqlite'
 
@@ -11,8 +12,8 @@ def fetch_data():
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     query = """
-    SELECT container, test_name, result
-    FROM runs;
+        SELECT container, test_name, result
+        FROM runs;
     """
     cursor.execute(query)
     rows = cursor.fetchall()
@@ -20,16 +21,24 @@ def fetch_data():
     return rows
 
 def process_data(rows):
-    # Dictionary to hold the counts of PASS results
     data = {}
+    false_negatives = set()
     for container, test_name, result in rows:
         test_base_name = "-".join(test_name.split("-")[:-1])  # Remove variant number
+        test_number = test_base_name.split("-")[-1]
+        positive_fails = POSITIVE_FAILS[test_number]
+        correct_result = 'PASS' if container not in positive_fails else 'FAIL'
         if container not in data:
             data[container] = {}
         if test_base_name not in data[container]:
             data[container][test_base_name] = 0
-        if result == 'PASS':
-            data[container][test_base_name] += 1
+        if container == 'defect-00_free.war' and result == 'FAIL':
+            false_negatives.add(test_name)
+        if result == correct_result:
+            if test_name not in false_negatives:
+                data[container][test_base_name] += 1
+            elif result != 'FAIL':
+                data[container][test_base_name] += 1
     return data
 
 def generate_heatmap(data):
@@ -47,7 +56,7 @@ def generate_heatmap(data):
     # Generate heatmap
     plt.figure(figsize=(10, 8))
     sns.heatmap(heatmap_data, annot=True, fmt="g", cmap="rocket_r", xticklabels=tests, yticklabels=containers, vmin=0, vmax=10)
-    plt.title('Test PASS Count Heatmap')
+    plt.title('Test correct result heatmap')
     plt.xlabel('Test Name')
     plt.ylabel('Container')
     plt.xticks(rotation=90, ha="right")

a4f04b659b9d9fe637774250e8921b62a10eff08
Milan Horínek <horinek@milanfon.cz>
2024-05-15 14:05:18 +0200

    Added token counters

---
+++ program/auxiliary/token_counter_codellama.py
@@ -0,0 +1,22 @@
+from transformers import CodeLlamaTokenizer
+import os
+
+FILES_DIR = "../generated/wizardcoder-python/"
+OUTPUT_COST = 120 #per milion tokens
+MODEL = "gpt-4"
+
+tokenizer = CodeLlamaTokenizer.from_pretrained("codellama/CodeLlama-7b-hf")
+token_count, file_count = 0, 0
+
+for filename in os.listdir(FILES_DIR):
+    file_path= os.path.join(FILES_DIR, filename)
+    if os.path.isfile(file_path) and filename.endswith('.robot'):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            token_count += len(tokenizer(file.read())['input_ids'])
+            file_count += 1
+
+def calc_price():
+    return (token_count / 1e6) * OUTPUT_COST
+
+
+print(f"Token count {token_count} in {file_count} files. Estimated output cost {calc_price()}.")
--- program/auxiliary/token_counter_mistral.py
+++ program/auxiliary/token_counter_mistral.py
@@ -1,18 +1,18 @@
-import tiktoken
+from transformers import AutoTokenizer
 import os
 
-FILES_DIR = "generated/openai-gpt4/"
-OUTPUT_COST = 2
+FILES_DIR = "../generated/mistral-v0.2/"
+OUTPUT_COST = 120 #per milion tokens
 MODEL = "gpt-4"
 
-encoding = tiktoken.encoding_for_model(MODEL)
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
 token_count, file_count = 0, 0
 
 for filename in os.listdir(FILES_DIR):
     file_path= os.path.join(FILES_DIR, filename)
     if os.path.isfile(file_path) and filename.endswith('.robot'):
         with open(file_path, 'r', encoding='utf-8') as file:
-            token_count += len(encoding.encode(file.read()))
+            token_count += len(tokenizer(file.read())['input_ids'])
             file_count += 1
 
 def calc_price():
